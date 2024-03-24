2120dade658660dedb910c7a3d1cf4d0d6a0828b
WmmyS <wesleymarcosm@gmail.com>
2024-03-23 21:13:29 -0300

    feat: Commit inicial.

---
+++ src/components/Form/ResultImc/style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+ resultImc: {
+  flex: 0,
+  marginTop: 25,
+  paddingTop: 60,
+  borderRadius: 50,
+  alignItems: "center",
+  width: "100%",
+ },
+ numberImc: {
+  fontSize: 18,
+  color: "#ff0043",
+  fontWeight: "bold",
+ },
+ information: {
+  fontSize: 18,
+  color: "#ff0043",
+  fontWeight: "bold",
+ }
+});
+
+export default styles
